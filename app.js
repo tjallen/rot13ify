b6a668fca749e840d908a5c7af2557403db4df77
Thom Allen <thomwork@gmail.com>
2018-01-11 10:28:49 +0000

    Add koa-response-time

--- app.js
+++ app.js
@@ -2,19 +2,13 @@ const Koa = require('koa');
 const app = new Koa();
 const views = require('koa-views');
 const logger = require('koa-logger');
+const responseTime = require('koa-response-time');
 
 const PORT = 3333;
 
 app.use(views(__dirname + '/views', { extension: 'pug' }));
 app.use(logger());
-
-app.use(async (ctx, next) => {
-  const start = new Date;
-  console.log(`started at ${start}`);
-  await next();
-  const ms = new Date - start;
-  console.log(`elapsed: ${ms}ms`);
-});
+app.use(responseTime());
 
 app.use(async(ctx, next) => {
   await next();
@@ -22,7 +16,6 @@ app.use(async(ctx, next) => {
 
 app.use(async (ctx) => {
   await ctx.render('index');
-  console.log('body was set');
 });
 
 app.listen(PORT);
