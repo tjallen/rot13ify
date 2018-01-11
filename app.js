e1e0c0a841b2c8aeda86629a17fd9e80f8523a54
Thom Allen <thomwork@gmail.com>
2018-01-11 11:54:54 +0000

    Add basic error handling middleware

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ const responseTime = require('koa-response-time');
 
 const PORT = 3333;
 
+// err handling
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    ctx.status = 400;
+    ctx.body = `error: ${err.message}`;
+    console.log(`error handler: ${err.message}`);
+  }
+});
+
 app.use(views(__dirname + '/views', { extension: 'pug' }));
 app
   .use(indexRouter.routes())
